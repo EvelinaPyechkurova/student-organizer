2b032b938b5f9d92363df3e3e8e83067548ef88c
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-02-08 03:46:42 +0200

    Added 'Subject' app, created 'Subject' model

--- organizer/settings.py
+++ organizer/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'subject.apps.SubjectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ subject/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Subject(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    image_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
