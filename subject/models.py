7651ef602b1ab368fc0a35df409de43245907289
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-02-17 19:43:33 +0200

    Added integrity constraint to 'Subject' model: one user can't have more that 200 subjects

--- subject/models.py
+++ subject/models.py
@@ -2,12 +2,29 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from django.core.exceptions import ValidationError
+
 class Subject(models.Model):
+    MAX_SUBJECTS_PER_USER = 200
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     image_url = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        super().clean()
+
+        if self.user.subject_set.count() >= self.MAX_SUBJECTS_PER_USER:
+            raise ValidationError(
+                message=f'You can\'t have more that {self.MAX_SUBJECTS_PER_USER} subjects per user', 
+                code='max_subjects_reached'
+            )
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
