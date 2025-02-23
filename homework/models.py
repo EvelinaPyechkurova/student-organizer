ade82e76a73366c317a84f0714d9e2da332a0bee
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-02-23 19:15:04 +0200

    Added validation to 'Homework' model: refactored time constraints validation; implemented custom save() method; added completion percent validation

--- homework/models.py
+++ homework/models.py
@@ -29,9 +29,18 @@ class Homework(models.Model):
     def clean(self):
         super().clean()
 
+        self.validate_percentage()
         self.validate_existence()
         self.validate_subject_consistency()
         self.validate_time_constraints()
+
+    
+    def validate_percentage(self):
+        if not (0 <= self.completion_percent <= 100):
+            raise ValidationError(
+                message='Completion percent must be between 0 and 100.',
+                code='invalid_completion_percent'
+            )
         
     
     def validate_subject_consistency(self):
@@ -83,8 +92,9 @@ class Homework(models.Model):
                         solution = 'To fix this, choose two lessons from the same subject.'
                     ),  
                     code='subject_mismatch'
-                )        
-        
+                )    
+
+
     def validate_existence(self):
 
         ERROR_MESSAGE = ('Homework must either have a {first_option} '
@@ -104,73 +114,110 @@ class Homework(models.Model):
             raise ValidationError(
                 message=ERROR_MESSAGE.format(
                     first_option='due at date',
-                    second_option='be due specific lesson'
+                    second_option='be due at a specific lesson'
                 ),
                 code='required'
             )
    
 
-
     def validate_time_constraints(self):
 
-        ERROR_MESSAGE = '{entity} can\'nt {} more than {days} days {direction}.'
+        ERROR_MESSAGE = '{entity} can\'t be set more than {days} days in the {direction}.'
 
         max_days = Homework.MAX_TIMEFRAME.days
 
         past_limit = now() - Homework.MAX_TIMEFRAME
         future_limit = now() + Homework.MAX_TIMEFRAME
         
-        if self.start_time and self.start_time < now() - Homework.MAX_TIMEFRAME:
-            raise ValidationError(
-                message='Homework can\'nt start more that {Homework.MAX_TIMEFRAME} in the past.',
-                code=''
-            )
-        
-        if self.lesson_given and self.lesson_given.start_time < now() - Homework.MAX_TIMEFRAME:
-            raise ValidationError(
-                message='Lesson homework was given at can\'nt start more that {Homework.MAX_TIMEFRAME} in the past.',
-                code=''
-            )
-       
-        if self.due_at and self.start_time > now() + Homework.MAX_TIMEFRAME:
-            raise ValidationError(
-                message='Homework can\'nt be due more that {Homework.MAX_TIMEFRAME} in the future.',
-                code=''
-            )
-        
-        if self.lesson_due and self.lesson_due.start_time > now() + Homework.MAX_TIMEFRAME:
-            raise ValidationError(
-                message='Lesson homework is due at can\'nt start more that {Homework.MAX_TIMEFRAME} in the future.',
-                code=''
-            )
-        
-        if self.start_time:
-            if self.due_at and self.start_time >= self.due_at:
+        for field, label in [
+            (self.start_time, 'Homework start time'),
+            (self.lesson_given.start_time if self.lesson_given else None, 'Lesson homework was given at'),
+        ]:
+            if field and field < past_limit:
                 raise ValidationError(
-                    message='Start time must be smaller then due_at',
-                    code=''
+                    message=ERROR_MESSAGE.format(
+                        entity=label,
+                        days=max_days,
+                        direction='past',
+                    ),
+                    code='past_limit_exceeded'
                 )
 
-            if self.lesson_due and self.start_time >= self.lesson_due.start_time:
+        for field, label in [
+            (self.start_time, 'Homework start time'),
+            (self.due_at, 'Homework due time'),
+            (self.lesson_due.start_time if self.lesson_due else None, 'Lesson homework is due to'),
+        ]:
+            if field and field > future_limit:
                 raise ValidationError(
-                    message='Start time must be smaller then lesson due start time',
-                    code=''
+                    message=ERROR_MESSAGE.format(
+                        entity=label,
+                        days=max_days,
+                        direction='future',
+                    ),
+                    code='future_limit_exceeded'
                 )
+                
+        if self.lesson_given and self.lesson_given.start_time > now():
+            raise ValidationError(
+                message='Homework can\'t be assigned to lesson starting in the future',
+                code='invalid_given_time'
+            )
+        
+        past_due_limit = now() - timedelta(days=30)
+
+        if self.due_at and self.due_at < past_due_limit:
+            raise ValidationError(
+                message="Homework due date can't be more than 30 days in the past.",
+                code="past_due_date"
+            )
+
+        fields = [
+            (self.due_at, 'homework due date'),
+            (self.lesson_due.start_time if self.lesson_due else None, 'lesson homework is due at')
+        ]
+
+        if self.start_time:
+
+            ERROR_MESSAGE = 'Homework start time must be earlier then {entity}.'
+
+            for field, label in fields:
+                if field and field <= self.start_time:
+                    raise ValidationError(
+                        message=ERROR_MESSAGE.format(
+                            entity=label
+                        ),
+                        code='invalid_start_order'
+                    )
+
 
         if self.lesson_given:
-            if self.due_at and self.lesson_given.start_time >= self.due_at:
-                raise ValidationError(
-                    message='Lesson given start_time time must be smaller then due_at',
-                    code=''
-                )
-            
-            if self.lesson_due and self.lesson_given.start_time >= self.lesson_due.start_time:
-                raise ValidationError(
-                    message='Lesson given start_time time must be smaller then lesson_due start_time',
-                    code=''
-                )
+
+            ERROR_MESSAGE = 'Lesson homework was given at start time must be earlier then {entity}.'
+
+            for field, label in fields:
+                if field and field <= self.lesson_given.start_time:
+                    raise ValidationError(
+                        message=ERROR_MESSAGE.format(
+                            entity=label
+                        ),
+                        code='invalid_start_order'
+                    )
 
 
     def save(self, *args, **kwargs):
         self.full_clean()
-        super().save()
+
+        if self.lesson_given or self.lesson_due:
+            self.subject = None
+
+        if not self.start_time and not self.lesson_given:
+            self.start_time = now()
+
+        if self.start_time and self.lesson_given and self.start_time == self.lesson_given.start_time:
+            self.start_time = None
+            
+        if self.due_at and self.lesson_due and self.due_at == self.lesson_due.start_time:
+            self.due_at = None
+
+        super().save(*args, **kwargs)
