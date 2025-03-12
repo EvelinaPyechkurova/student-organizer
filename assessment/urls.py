bf367d44b92797af3726e635860ea4676425af50
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-03-12 14:20:47 +0200

    Refactored assessment templates; added links

--- assessment/urls.py
+++ assessment/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path(route='', view=views.AssessmentListView.as_view(), name='assessment_list'),
-    path(route='<int:pk>', view=views.AssessmentDetailView.as_view(), name='assessmebt_detail'),
+    path(route='<int:pk>', view=views.AssessmentDetailView.as_view(), name='assessment_detail'),
 ]
