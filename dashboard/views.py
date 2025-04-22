1f8b23ebd4189fa606118bd8ac368b8448f389f8
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-04-22 20:20:36 +0300

    Added authorization for UserProfile and Dashboard views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 
 
-class DashboardView(View):
+class DashboardView(LoginRequiredMixin, View):
     template_name = 'dashboard.html'
 
     def get(self, request):
--- userprofile/profile_views.py
+++ userprofile/profile_views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
     
 
-class ProfileUpdateView(UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = User
 
-class ProfileDetailView(DetailView):
+class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = User
 
-class ProfileDeleteView(DeleteView):
+class ProfileDeleteView(LoginRequiredMixin, DeleteView):
     model = User
