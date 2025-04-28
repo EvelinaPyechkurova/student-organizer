a918ccc3299da54c84493956bcc99c92d2395212
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-04-28 20:19:32 +0300

    Improved profile detail view template; edited navbar

--- userprofile/profile_views.py
+++ userprofile/profile_views.py
@@ -13,9 +13,27 @@ class ProfileDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['profile'] = self.request.user.userprofile
+        profile = self.request.user.userprofile
+        context['profile'] = profile
         context['model_name'] = 'profile'  # logical entity is "profile" even though model is User
         print(context)
+        context['reminders'] = [
+            {
+                'label': 'Lesson reminders',
+                'enabled': profile.recieve_lesson_remainders,
+                'timing': profile.lesson_remainder_timing,
+            },
+            {
+                'label': 'Assessment reminders',
+                'enabled': profile.recieve_assessment_remainders,
+                'timing': profile.assessment_remainder_timing,
+            },
+            {
+                'label': 'Homework reminders',
+                'enabled': profile.recieve_homework_remainders,
+                'timing': profile.homework_remainder_timing,
+            },
+        ]
         return context
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
