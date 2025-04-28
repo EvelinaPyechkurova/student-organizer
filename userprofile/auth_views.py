457c3aa38bef810bda77a910f5b15f0f71803310
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-04-28 23:13:44 +0300

    Added UpdateView to UserProfile

--- userprofile/auth_views.py
+++ userprofile/auth_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -14,10 +15,11 @@ class UserRegisterView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['cancel_link'] = reverse_lazy('user_login')
+        context['cancel_link'] = reverse_lazy('profile_update')
         return context
 
     def form_valid(self, form):
         responce = super().form_valid(form)
         UserProfile.objects.create(user=self.object)
+        login(self.request, self.object)
         return responce
--- userprofile/profile_views.py
+++ userprofile/profile_views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import DetailView, DeleteView
 from django.urls import reverse_lazy
 
 from utils.mixins import UserObjectMixin
+
+from .forms import UserUpdateForm, ProfileUpdateForm
     
 
 class ProfileDetailView(LoginRequiredMixin, UserObjectMixin, DetailView):
@@ -35,8 +39,43 @@ class ProfileDetailView(LoginRequiredMixin, UserObjectMixin, DetailView):
         return context
 
 
-class ProfileUpdateView(LoginRequiredMixin, UserObjectMixin, UpdateView):
-    model = User
+class ProfileUpdateView(LoginRequiredMixin, View):
+    template_name = 'profile/profile_update.html'
+    success_url = reverse_lazy('profile_detail')
+    cancel_link = reverse_lazy('profile_detail')
+
+    def get(self, request, *args, **kwargs):
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.userprofile)
+
+        return render(
+            request, 
+            self.template_name,
+            {
+                'user_form': user_form,
+                'profile_form': profile_form,
+                'cancel_link': self.cancel_link
+            }
+        )
+    
+    def post(self, request, *args, **kwargs):
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, instance=request.user.userprofile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect(self.success_url)
+        
+        return render(
+            request, 
+            self.template_name,
+            {
+                'user_form': user_form,
+                'profile_form': profile_form,
+                'cancel_link': self.cancel_link
+            }
+        )
 
 
 class ProfileDeleteView(LoginRequiredMixin, UserObjectMixin, DeleteView):
