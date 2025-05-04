17ed50c51b4091348b88ea2bc3a188f3eee6e9da
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-05-05 02:33:37 +0300

    Started working on calendar

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,14 +2,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 
+from datetime import date
+import calendar
+
 
 class DashboardView(LoginRequiredMixin, View):
     template_name = 'dashboard/dashboard.html'
 
     def get(self, request):
         user = request.user
+        today = date.today()
+        year = today.year
+        month = calendar.month_name[today.month]
+
         context = { 
-            'first_name': user.first_name
+            'year': year,
+            'month': month,
         }
-        print(context)
+
         return render(request, self.template_name, context)
